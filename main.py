1ccf5e122bbc3357ed23b144f552a5cfdb4e5c41
E Vishnu prasad <109452822+VSeekk@users.noreply.github.com>
2024-06-26 18:31:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+CLIENT_ID ="5a0afb31a05d4117ae7e8f6468f49d0b"
+CLIENT_SECRET="a4834b1dbf9f4b3d8d423668a394afbd"
+USER_NAME="31lrss2bsepcti3cvhvkei235dmi"
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+#using  Beautiful soup to extract the list of top 100 songs in billboard on a particular date
+from bs4 import BeautifulSoup
+import requests
+
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
+
+#API creation
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt",
+        username=USER_NAME,
+    )
+)
+
+#creating URI links list for the billBored 100 songs
+user_id = sp.current_user()["id"]
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    # print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+
+#creating a  playlist and adding songs to it
+playlist = sp.user_playlist_create(user=USER_NAME, name=f"{date} Billboard 100", public=False)
+# print(playlist)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
